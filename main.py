737c6b336ca4f4adf9cd544ab91fa61fbffcc2fc
Ilya <ilya_0836@mail.ru>
2023-10-15 18:57:24 +0300

    calendar v1

---
+++ main.py
@@ -0,0 +1,48 @@
+import time
+import schedule
+from telebot.async_telebot import AsyncTeleBot
+from threading import Thread
+import asyncio
+
+with open('bot.txt') as f:
+    TOKEN = f.read()
+
+bot = AsyncTeleBot(TOKEN)
+chats = []
+
+
+@bot.message_handler(commands=['start', 'calendar'])
+async def start(message):
+    from parse import text
+
+    chat_id = message.chat.id
+    if chat_id not in chats:
+        chats.append(chat_id)
+    await bot.send_message(chat_id, text)
+
+
+@bot.message_handler(commands=['stop'])
+async def stop(message):
+    chat_id = message.chat.id
+    if chat_id in chats:
+        del chats[chat_id]
+        await bot.send_message(chat_id, 'Вы отписались от рассылки')
+
+
+async def send():
+    from parse import text
+
+    for chat_id in chats:
+        await bot.send_message(chat_id, text)
+
+
+def run():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    schedule.every(1).day.at('00:01:10').do(send)
+    Thread(target=run).start()
+    asyncio.run(bot.polling(none_stop=True, interval=0))
---
+++ parse.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pymorphy2
+import requests
+import time
+
+driver = webdriver.Firefox()
+morph = pymorphy2.MorphAnalyzer()
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0'}
+text = '''"Календарь"
+Сегодня '''
+
+url_events = 'https://kakoysegodnyaprazdnik.ru/'
+while True:
+    try:
+        driver.get(url_events)
+        main_page = driver.window_handles[0]
+        driver.switch_to.window(main_page)
+        time.sleep(5)
+        break
+    except Exception:
+        driver.quit()
+        driver = webdriver.Firefox()
+web_events = BeautifulSoup(driver.page_source, 'lxml')
+driver.quit()
+
+url_weather_spb = 'https://www.gismeteo.ru/weather-sankt-peterburg-4079/?ysclid=lmhwisjcfq453695344'
+response2 = requests.get(url_weather_spb, headers=headers)
+web_weather_spb = BeautifulSoup(response2.text, 'lxml')
+
+url_weather_syzran = 'https://www.gismeteo.ru/weather-syzran-4448/'
+response3 = requests.get(url_weather_syzran, headers=headers)
+web_weather_syzran = BeautifulSoup(response3.text, 'lxml')
+
+today = web_events.find('h2', attrs={'class': 'mainpage'})
+today = today.text.split()
+data = ' '.join(today[:3])
+day = today[-1]
+text += f'{data}, {day}.\n'
+
+astro_times = web_weather_spb.find('div', attrs={'class': 'astro-times'}).find_all('div')
+times = '\n'.join([astro_times[1].text, astro_times[2].text])
+
+text += f'\nСолнце:\n{times}\n\nСобытия:\n'
+
+events = web_events.findAll('span', attrs={'itemprop': 'text'})
+
+for i in range(len(events) - 2):
+    q = events[i].text
+    n = q.find('(')
+    m = q.find(')')
+    if n != -1:
+        q = q[:n - 1] + q[m + 1:]
+    text += f'* {q}\n'
+
+text += '\nИменинники:\n'
+
+r = events[-2].text.split()
+for i in range(2, len(r)):
+    t = r[i][:-1]
+    word = morph.parse(t)[0]
+    gent = word.inflect({'nomn'})
+    gent = gent.word
+    text += f'~ {gent.capitalize()}\n'
+
+weather_syzran = web_weather_syzran.find('div', attrs={'class': 'chart'}).find_all('span', attrs={
+    'class': 'unit_temperature_c'})
+for i in range(len(weather_syzran)):
+    weather_syzran[i] = int(weather_syzran[i].text[1:])
+text += f'\nПогода в Сызрани: +{max(weather_syzran)}°С\nВ. И. Ленин спит? Да, спит.'
